7b5de9f7501575a27eb3fb6ea406ad43e4f86873
Enrico404 <sedoni.enrico@gmail.com>
2018-08-26 16:26:00 +0200

    GUI improvements and bug fix

--- ClientFrame.py
+++ ClientFrame.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import *
 from MyClient import  Client
-from PyQt5.QtCore import pyqtSignal
+from PyQt5 import QtGui
+from PyQt5 import QtCore
 from threading import Thread
 
 
@@ -10,17 +11,48 @@ class ClientFrame(QWidget):
         super().__init__()
         self.cl = Client()
         self.log = False
-
+        self.stylesheet = """
+            QPushButton{
+                border-radius: 12px;
+                background-color: #3498db;
+                color: white;
+            }
+            QPushButton:hover{
+                background-color: #2980b9;
+                
+            }
+            
+            QTextEdit{
+                border:1px solid gray;
+                color: black;
+                
+            }
+            
+            QLineEdit{
+                padding-left: 10px;
+                border:1px solid gray;
+                border-radius: 12px;
+            }
+            QLineEdit:hover{
+                
+                border:1px solid #3498db;
+                border-radius: 14px;
+               
+            }
+        
+        """
 
 
 
     def init_window(self):
         self.setWindowTitle("Simple Chat")
-        self.setGeometry(300, 300, 600, 600)
+        self.move(300, 300)
+        self.setFixedSize(600,600)
         self.show()
         self.init_gui()
 
     def init_gui(self):
+        self.setStyleSheet(self.stylesheet)
         self.textview = QTextEdit(self)
         self.textview.move(15, 15)
         self.textview.resize(570, 500)
@@ -31,24 +63,34 @@ class ClientFrame(QWidget):
         self.cl.update.connect(self.textview.append)  # connetto il segnale definito nella classe myclient e appendo il testo alla textview
 
         self.text_message = QLineEdit(self)
-        self.text_message.setGeometry(15, 540, 430, 30)
+        self.text_message.setGeometry(15, 540, 440, 30)
         self.text_message.show()
+
+
+
 
         self.send = QPushButton("Send", self)
         self.send.setGeometry(483 , 540, 100, 30)
         self.send.clicked.connect(self.sendClick)
         self.send.show()
 
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Enter or QtCore.Qt.Key_Return:
+            if self.text_message.text() != "":
+                self.sendClick()
 
     def start(self):
         self.cl.log(self.text_message.text(),self.textview)
         self.log = True
+        self.text_message.setText("")
+
 
     def sendClick(self):
         if self.log == False:
             print("loggo...")
             self.start()
         text = self.text_message.text()
+        self.text_message.setText("")
         self.cl.send_message(text)
 
 
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Follow these three steps:
 1) open a terminal and launch the server: python3 ServerRun.py
 2) open another terminal and launch the first client: python3 ClientRun.py
 3) open another terminal and launch the second client: python3 ClientRun.py
+
+NB: you must have installed python3, python3-pyqt5 for run the application
